9fa13dbd411a86f18c25ad7f8a4c16116df9818a
Mark <sim7rok@gmail.com>
2021-08-19 21:53:01 +0300

    Lesson 3 class work and practice

---
+++ Lesson3/class_practice.py
@@ -0,0 +1,55 @@
+import string
+
+print('Завдання 1')
+my_str_1 = 'hello'
+my_str_1 = my_str_1[::-1]
+print(my_str_1)
+
+print('Завдання 2')
+my_str_2 = input('Введіь будь яку стрічку: ')
+print(f'Після видалення першого і останнього символу, стрічка виглядає так: {my_str_2[1:-1]}')
+
+print('Завдання 3')
+my_str_3 = input('Введіь будь яку стрічку: ')
+letter_one = my_str_3[:1]
+letter_two = my_str_3[-1:]
+print(f'{letter_two}{my_str_3[1:-1]}{letter_one}')
+
+print('Завдання 4')
+my_str_4 = 'HeLLo My naMe is jack'
+my_str_4 = my_str_4.lower().capitalize()
+my_str_4 = my_str_4.split(' ')
+my_str_4[4] = my_str_4[4].capitalize()
+my_str_4 = ' '.join(my_str_4)
+print(my_str_4)
+
+print('Завдання 5')
+my_str_5 = my_str_4
+my_str_5 = my_str_5.split(' ')
+my_str_5 = ''.join(my_str_5)
+print(my_str_5)
+
+print('Завдання 6')
+my_str_6 = 'HelloWorlD'
+print(f'{my_str_6[:5]} {my_str_6[5:].lower().capitalize()}')
+
+print('Завдання 7')
+print(''.join(s for s in 'amazing' if s not in 'aoyeui'))
+
+print('Завдання 8')
+loop = True
+while loop:
+    my_str_8 = input('Введіь нікнейм: ')
+    if len(my_str_8) > 16 or len(my_str_8) < 4:
+        print('довжина нікнейму повинна бути в межах 4-16 символів')
+        continue
+    for digits in string.digits:
+        if digits in my_str_8:
+            print('Нікнейм не повиненм містити цифри')
+            continue
+    for symb in string.punctuation:
+        if symb in my_str_8:
+            print('Нікнейм не повиненм містити спец.символи')
+            continue
+    print(f'Ваш нікнейм: {my_str_8}')
+    loop = False
---
+++ Lesson3/class_work.py
@@ -0,0 +1,45 @@
+print('start')
+# print(chr(99))
+# print(ord('a'))
+
+'''довжина стрічки'''
+# value = 'string in python'
+# print(len(value))
+
+'''перебір символів стрічки'''
+# i = 1
+# for letter in value:
+#     print(letter, i)
+#     i += 1
+
+'''зріз'''
+# print(value[10:])
+
+# i = 1
+# var = 'one two three four five'
+# for letter in var:  # цикл щоб швидко підглянути нумерацію символів :)
+#     print(letter, i)
+#     i += 1
+# print(var[:3])
+# print(var[8:14])
+# print(var[8:19])
+# print(var[19:])
+# print(var[::-1])
+# print(var[::2])
+ins = ' insert '
+S = f'qwerty{ins}qwerty'
+
+# print(S.capitalize())  # Робить перший симовол великим
+# print(S.count('q', 3, 6))  # Ви водить к-сть вказаних символів у стрічці або її елементі
+# print(S.endswith('ty'))  # Вказує чи закінчуєть стрічка на вказані символи
+# print(S.find('rt'))  # Шукає в стрічці символ або елемент та повертає початковий індекс
+# print(S.index('r'))  # Шукає символ в стрічці та повертає його індекс
+# print(S.isalpha())  # Перевіряє чи стрічка складається тільки з літер
+# print(S.isdigit())  # Перевіряє чи стрічка складається тільки з чисел
+# print(S.lower())  # Робить всі символи в стрічці малими
+# print(S.replace('ver', 'rev'))  # Замінює вказаний елемент в стрічці
+
+# text_1 = 'MYBNAMEBPASSWORDB11'
+# print(' '.join(text_1.split('B')))
+
+
